f371eb2ead151d160b4c804ea11878def9c57896
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-15 07:03:10 +0530

    Added displayBooks() in UI class

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'Anshu Gupta',
+                isbn: '12345'
+            },
+            {
+                title: 'Book Two',
+                author: 'Ashutosh Singh',
+                isbn: '23456'
+            },
+            {
+                title: 'Book Three',
+                author: 'Rahul Kumar Ojha',
+                isbn: '34567'
+            }
+        ];
+        const books = StoredBooks;
+        books.forEach((book) => UI.addBookToList(book));
+    }
+}
+
 // Store Class: Handles Storage
 // Event: Display books
 // Event: Add a book
