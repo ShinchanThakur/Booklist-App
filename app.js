f257bfdeafeae7bc7d3d208637110da203a0ede3
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-15 06:57:17 +0530

    Book class added

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // Book Class: Represents a book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
 // UI Class: Handle UI Tasks
 // Store Class: Handles Storage
 // Event: Display books
